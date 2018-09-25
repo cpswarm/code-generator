6d24299c859f68259450997ff891fe602860cf8b
gprato <gianluca.prato@ismb.it>
2018-09-25 23:53:27 +0200

    fixed in/out files paths

--- src/main/java/it/ismb/pert/codegen/App.java
+++ src/main/java/it/ismb/pert/codegen/App.java
@@ -7,29 +7,39 @@ import it.ismb.pert.codegen.scxml.SCXML2RosGenerator;
 public class App {
 
 	private final static String TEMPLATE_FILE = "template/state_machine.vm";
-	private final static String INPUT_DIR = "input/scxml/";
-	private final static String OUTPUT_DIR = "generated/";
+//	private final static String INPUT_DIR = "input/scxml/";
+//	private final static String OUTPUT_DIR = "generated/";
 
 	public static void main(String[] args) {
 
-		String inputFile = "input/scxml/UAV_sar_FSM.xml";
-		String outputFile = "generated/uav_behavior.py";
+//		String inputFile = "/home/gprato/eclipse-workspace/cpswarm/code-generator/input/scxml/UAV_sar_FSM3.xml";
+//		String outputFile = "/home/gprato/eclipse-workspace/cpswarm/code-generator/generated/uav_behavior_test.py";
+
+		// String inputFile = "input/scxml/UGV_sar_FSM.xml";
+		// String outputFile = "generated/ugv_behavior.py";
+
+		// String inputFile = "input/scxml/logistic_FSM.xml";
+		// String outputFile = "generated/logistic_behavior.py";
 
 		int i = 0;
 		String arg;
 
 		while (i < args.length && args[i].startsWith("-")) {
 			arg = args[i++];
-			if (arg.equals("-f")) {
-				if (i < args.length)
-					inputFile = INPUT_DIR + args[i++];
-				else
-					System.err.println("-f requires a filename");
+			if (arg.equals("-src")) {
+				if (i < args.length) {
+					// inputFile = INPUT_DIR + args[i++];
+					inputFile = args[i++];
+				} else {
+					System.err.println("-src requires a file path");
+				}
 			} else if (arg.equals("-o")) {
-				if (i < args.length)
-					outputFile = OUTPUT_DIR + args[i++];
-				else
-					System.err.println("-o requires a filename");
+				if (i < args.length) {
+					// outputFile = OUTPUT_DIR + args[i++];
+					outputFile = args[i++];
+				} else {
+					System.err.println("-o requires a file path");
+				}
 			}
 		}
 
@@ -43,9 +53,5 @@ public class App {
 		}
 		System.out.println("Code generated with success!!");
 	}
-
-	// private void init() {
-	//
-	// }
 
 }
