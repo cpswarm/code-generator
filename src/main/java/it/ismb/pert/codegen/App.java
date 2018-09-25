3df150714016b8ecd6467d652069f7ebb34950d4
gprato <gianluca.prato@ismb.it>
2018-09-25 23:54:23 +0200

    fixed in/out files paths 2

--- src/main/java/it/ismb/pert/codegen/App.java
+++ src/main/java/it/ismb/pert/codegen/App.java
@@ -20,6 +20,8 @@ public class App {
 
 		// String inputFile = "input/scxml/logistic_FSM.xml";
 		// String outputFile = "generated/logistic_behavior.py";
+		
+		String inputFile, outputFile;
 
 		int i = 0;
 		String arg;
