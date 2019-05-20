8ac1acd73830561606700c630ec7a27d53c2fd0d
gprato <gianluca.prato@ismb.it>
2019-05-20 16:00:17 +0200

    added --env input cmd, only ROS support

--- src/main/java/it/links/pert/codegen/App.java
+++ src/main/java/it/links/pert/codegen/App.java
@@ -1,12 +1,13 @@
 package it.links.pert.codegen;
 
+import it.links.pert.codegen.generator.CodeGenerator;
 import it.links.pert.codegen.scxml.SCXML2RosGenerator;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		String inputPath = null, outputDir = null;
+		String inputPath = null, outputDir = null, runtimeEnv = null;
 
 		int i = 0;
 		String arg;
@@ -25,15 +26,30 @@ public class App {
 				} else {
 					System.err.println("--target requires a directory path");
 				}
+			} else if (arg.equals("--env")) {
+				if (i < args.length) {
+					runtimeEnv = args[i++];
+				} else {
+					System.err.println("--env requires a valid Runtime Environment (e.g. ROS)");
+				}
 			}
 		}
+		
+		CodeGenerator generator = null;
+		
+		switch(runtimeEnv) {
+			case "ROS":
+				generator =  new SCXML2RosGenerator(inputPath, outputDir);
+				break;
+			default:
+				generator =  new SCXML2RosGenerator(inputPath, outputDir);
+		}
 
-		SCXML2RosGenerator generator = new SCXML2RosGenerator(inputPath, outputDir);
 		// URL url = App.class.getClassLoader().getResource(stateMachineFile);
 		if(generator.generate())
 			System.out.println("Code generated with success!!");
 		else
-			System.out.println("An error occured during code generetion");
+			System.out.println("An error occured during code generation");
 	}
 
 }
