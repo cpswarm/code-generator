4b356874d5f30864b8aea5eb36db410315340379
Gianluca Prato <gianluca.prato@linksfoundation.com>
2019-10-17 13:14:24 +0200

    modified tests according to code refactoring

--- src/test/java/it/links/pert/codegen/scxml/SCXML2RosTests.java
+++ src/test/java/it/links/pert/codegen/scxml/SCXML2RosTests.java
@@ -1,6 +1,7 @@
 package it.links.pert.codegen.scxml;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,25 +19,23 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 class SCXML2RosTests {
 
-	private static final String INPUT_PATH = "/it/links/pert/codegen/scxml/data/UAV_sar_FSM.xml";
+	private static final String INPUT_PATH = "/it/links/pert/codegen/scxml/data/UAV_sar_FSM2.xml";
 	private static final String OUTPUT_DIR = "test_tmp/";
 	private static File testDirectory;
 	private static SCXML2RosGenerator generator;
 
 	@BeforeAll
 	static void setUp() {
+		//Set up temp directory to put outputs in
 		testDirectory = new File(OUTPUT_DIR);
 		if (!testDirectory.exists()) {
 			testDirectory.mkdir();
 		}
-	}
-	
-	@BeforeAll
-	static void createGeneratorInstance() {
-		final Path resourceDirectory = Paths.get("src","test","resources");
+		//Initialize @SCXML2RosGenerator instance
+		final Path resourceDirectory = Paths.get("src", "test", "resources");
 		generator = new SCXML2RosGenerator(resourceDirectory + INPUT_PATH, OUTPUT_DIR);
 	}
-	
+
 	@AfterAll
 	static void tearDown() {
 		if (testDirectory.exists()) {
@@ -52,23 +51,23 @@ class SCXML2RosTests {
 	@Test
 	@Order(1)
 	public void testCreateROSPackage() {
-		assertTrue(generator.createROSPackage());
+		assertTrue(generator.createNewROSPackage());
 	}
-	
+
 	@Test
 	@Order(2)
 	public void testCreateROSPackageWithExistingDir() {
 		final File beahaviorPkg = new File(OUTPUT_DIR + SCXML2RosGenerator.ROS_PKG_DEAFULT_NAME);
-		if(beahaviorPkg.exists())
-			assertTrue(generator.createROSPackage());
+		if (beahaviorPkg.exists())
+			assertTrue(generator.createNewROSPackage());
+		else {
+			fail("Default package should already exist");
+		}
 	}
 
 	@Test
 	@Order(3)
 	public void testGenerate() {
-		generator.generate();
+		assertTrue(generator.generate());
 	}
-	
-	
-
 }
