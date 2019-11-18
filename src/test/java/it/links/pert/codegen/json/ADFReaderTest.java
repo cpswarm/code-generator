b84077a04e3df95a32d0a62b37dc0d8333d79566
Gianluca Prato <gianluca.prato@linksfoundation.com>
2019-11-18 18:28:49 +0100

    improved ADFReader tests

--- src/test/java/it/links/pert/codegen/json/ADFReaderTest.java
+++ src/test/java/it/links/pert/codegen/json/ADFReaderTest.java
@@ -1,7 +1,9 @@
 package it.links.pert.codegen.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,25 +22,27 @@ class ADFReaderTest {
 	private static final String INPUT_PATH = "/it/links/pert/codegen/json/data/uav_ADF_test1.json";
 	private final Path resourceDirectory = Paths.get("src", "test", "resources");
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ADFReader.class.getName());
+	private static final Logger LOGGER = LoggerFactory.getLogger(ADFReaderTest.class.getName());
 
 	@Test
-	public void testADFRead() throws Exception {
+	public void testADFRead() {
 		LOGGER.info("-----------------------------------------------------------------------------------------");
 		LOGGER.info("--------------------Starting testADFRead test--------------------------------------------");
 		LOGGER.info("-----------------------------------------------------------------------------------------");
-
-		final Reader ADFileReader = Files.newBufferedReader(Paths.get(resourceDirectory + INPUT_PATH));
-		JsonArray functionList = ADFReader.read(ADFileReader);
-		assertEquals(3, functionList.size());
-		JsonObject function0 = (JsonObject) functionList.get(0);
-		assertEquals("uav_mavros_takeoff", function0.get(FUNCTION_NAME).getAsString());
-		JsonObject function1 = (JsonObject) functionList.get(1);
-		assertEquals("uav_mavros_land", function1.get(FUNCTION_NAME).getAsString());
-		JsonObject function2 = (JsonObject) functionList.get(2);
-		assertEquals("uav_assign_task", function2.get(FUNCTION_NAME).getAsString());
-		JsonObject fnc_api = function2.get("api").getAsJsonObject();
-		assertEquals("rosaction", fnc_api.get("comm_paradigm").getAsString());
+		try (Reader ADFileReader = Files.newBufferedReader(Paths.get(resourceDirectory + INPUT_PATH))) {
+			JsonArray functionList = ADFReader.read(ADFileReader);
+			assertEquals(3, functionList.size());
+			JsonObject function0 = (JsonObject) functionList.get(0);
+			assertEquals("uav_mavros_takeoff", function0.get(FUNCTION_NAME).getAsString());
+			JsonObject function1 = (JsonObject) functionList.get(1);
+			assertEquals("uav_mavros_land", function1.get(FUNCTION_NAME).getAsString());
+			JsonObject function2 = (JsonObject) functionList.get(2);
+			assertEquals("uav_assign_task", function2.get(FUNCTION_NAME).getAsString());
+			JsonObject fnc_api = function2.get("api").getAsJsonObject();
+			assertEquals("rosaction", fnc_api.get("comm_paradigm").getAsString());
+		} catch (IOException e) {
+			fail("Error:", e);
+		}
 	}
 
 }
