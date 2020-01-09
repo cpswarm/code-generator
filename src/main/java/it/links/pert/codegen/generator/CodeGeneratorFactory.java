209b4a1f4593c080474ff281155905543f56ad0f
Gianluca Prato <gianluca.prato@linksfoundation.com>
2020-01-09 15:40:30 +0100

    added null check

--- src/main/java/it/links/pert/codegen/generator/CodeGeneratorFactory.java
+++ src/main/java/it/links/pert/codegen/generator/CodeGeneratorFactory.java
@@ -6,16 +6,17 @@ import it.links.pert.codegen.scxml.SCXML2RosGenerator;
 import it.links.pert.codegen.scxml.SCXML2RosSimulationMode;
 
 public class CodeGeneratorFactory {
-	
+
 	/**
+	 * Factory to instantiate {@link CodeGenerator}
 	 * 
-	 * @param type
-	 * @param outputDir
-	 * @param options
-	 * @return
+	 * @param type      CodeGenerator type (e.g CodeGeneratorType.SCXML2ROS)
+	 * @param outputDir Output directory path
+	 * @param options   additional options dependent on {@link CodeGeneratorType}
+	 * 
+	 * @return a {@link CodeGenerator} instance
 	 */
-	public static CodeGenerator getInstance(CodeGeneratorType type, String outputDir,
-			Map<String, String> options) {
+	public static CodeGenerator getInstance(CodeGeneratorType type, String outputDir, Map<String, String> options) {
 		CodeGenerator generator = null;
 
 		switch (type) {
@@ -26,18 +27,17 @@ public class CodeGeneratorFactory {
 			} else {
 				generator = new SCXML2RosGenerator(options.get("scxmlPath"), options.get("adfPath"), outputDir);
 			}
-			if(options.containsKey("mode")) {
-				if("simulation".contentEquals(options.get("mode"))) {
+			if (options.containsKey("mode")) {
+				String mode = options.get("mode");
+				if (mode != null && "simulation".contentEquals(mode)) {
 					generator = new SCXML2RosSimulationMode((SCXML2RosGenerator) generator);
 				}
 			}
 			break;
-
 		default:
 			// TODO throw dedicated exception
 			break;
 		}
-
 		return generator;
 	}
 }
